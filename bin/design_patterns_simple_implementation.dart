06cf6af41fa95a1b23885cafc0ef6aa10c00db2e
Saeed Fekri <en.saeedfekri@gmail.com>
2025-01-06 17:52:02 +0330

    delete unused files

--- bin/design_patterns_simple_implementation.dart
+++
@@ -1,5 +0,0 @@
-import 'package:design_patterns_simple_implementation/design_patterns_simple_implementation.dart' as design_patterns_simple_implementation;
-
-void main(List<String> arguments) {
-  print('Hello world: ${design_patterns_simple_implementation.calculate()}!');
-}
